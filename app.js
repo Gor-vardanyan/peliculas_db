d7417cfe7e7d1a053ea3a226aa1c5779fa50c174
Gor <g.var.design@gmail.com>
2020-10-16 12:43:32 +0200

    app js changes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const auth = require('./middleware/auth');
 const movieRouter = require('./route/movie');
 const userRouter = require('./route/user');
 const orderRouter = require('./route/order');
-const config = require('config/config.json')[env];
+//const config = require('config/config.json')[env];
 const app = express();
 const PORT = 3000;
 
@@ -13,10 +13,10 @@ app.use('/user', userRouter);
 app.use('/movie', movieRouter);
 app.use('/order', orderRouter);
 
-if (config.use_env_variable) {
+/*if (config.use_env_variable) {
     sequelize = new Sequelize(process.env[config.use_env_variable], config); 
 } else {
      sequelize = new Sequelize(config.database, config.username, config. password, config); 
   }
-
+*/
 app.listen(PORT, console.log("Server runing on port " + PORT));
